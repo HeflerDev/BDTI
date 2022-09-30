8145e0a03e0f4114052454af1913f95872432e37
HeflerDev <heflerdev@gmail.com>
2022-09-29 21:24:51 -0300

    BDTI - Adjust task component

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import {Tasks} from "../lib/Tasks";
+import {Col, Row} from "react-bootstrap";
+import {Important} from "../assets/Icons";
 
-const Task = ({tasks}:{tasks: Tasks}) => (
-    <div className="task-container">
-        <div className="repeat">{tasks.repeat}</div>
-        <div className="completed">{tasks.completed}</div>
-        <div className="title">{tasks.title}</div>
-        <div className="important">{tasks.important}</div>
+const Task = ({tasks}: { tasks: Tasks }) => (
+    <Row className="task-container">
+        <div className="repeat">{tasks.repeat.toUpperCase()}</div>
+        <Col xs={3} className={"check-container"}>
+            <div className={tasks.completed ? "completed" : "incomplete"}></div>
+        </Col>
+        <Col xs={9} className={"text-container"}>
+            <Row>
+                <Col xs={10} className={"text-container"}>
+                    <div className="title">{tasks.title}</div>
+                </Col>
+                <Col xs={2} className={"important-container"}>
+                    <div className="important"><Important size={"2rem"}/></div>
+                </Col>
+            </Row>
+            <p className="description">{tasks.description}</p>
+        </Col>
+        <Col xs={6}>
+            <div className="dropdown"></div>
+        </Col>
         <div className="tag-list">{tasks.tags}</div>
-        <div className="dropdown"></div>
-    </div>
+    </Row>
 )
 
 export default Task;
